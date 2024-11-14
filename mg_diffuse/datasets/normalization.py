Fixed bugs with normalization

mg_diffuse/datasets/normalization.py
@@ -48,8 +48,8 @@ class Normalizer:
             self.maxs = np.array(params['maxs'], np.float32)
             self.X = None
         else:
-            self.mins = X.min(axis=0)
-            self.maxs = X.max(axis=0)
+            self.mins = X.min(axis=(0, 1))
+            self.maxs = X.max(axis=(0, 1))
             self.X = X.astype(np.float32)
             self.params = {
                 'mins': list(self.mins),

mg_diffuse/datasets/sequence.py
@@ -7,46 +7,48 @@ import os
 
 from tqdm import tqdm
 
-from .normalization import  *
+from .normalization import *
 
-Batch = namedtuple('Batch', 'trajectories conditions')
+Batch = namedtuple("Batch", "trajectories conditions")
 
 
 def read_trajectory(sequence_path):
-    with open(sequence_path, 'r') as f:
+    with open(sequence_path, "r") as f:
         lines = f.readlines()
 
     trajectory = []
 
     for line in lines:
-        state = line.split(',')
+        state = line.split(",")
         state = [float(s) for s in state]
 
         trajectory.append(state)
 
     return trajectory
 
-def load_trajectories(dataset, parallel=True):
+
+def load_trajectories(dataset, parallel=True, fnames=None):
     """
-        load dataset from directory
+    load dataset from directory
     """
 
-    trajectories_path = os.path.join('data_trajectories', dataset, 'trajectories')
+    trajectories_path = os.path.join("data_trajectories", dataset, "trajectories")
 
-    try:
-        fnames = os.listdir(trajectories_path)
-    except FileNotFoundError:
-        raise ValueError(f'Could not find dataset at {trajectories_path}')
+    if fnames is None:
+        try:
+            fnames = os.listdir(trajectories_path)
+        except FileNotFoundError:
+            raise ValueError(f"Could not find dataset at {trajectories_path}")
 
     trajectories = []
 
-    print(f'[ datasets/sequence ] Loading trajectories from {trajectories_path}')
+    print(f"[ datasets/sequence ] Loading trajectories from {trajectories_path}")
 
     fpaths = [os.path.join(trajectories_path, fname) for fname in fnames]
 
     if not parallel:
         for fpath in tqdm(fpaths):
-            if not fpath.endswith('.txt'):
+            if not fpath.endswith(".txt"):
                 continue
             trajectories.append(read_trajectory(fpath))
     else:
@@ -54,7 +56,9 @@ def load_trajectories(dataset, parallel=True):
 
         # read trajectories in parallel with tqdm progress bar
         with mp.Pool(cpu_count()) as pool:
-            trajectories = list(tqdm(pool.imap(read_trajectory, fpaths), total=len(fpaths)))
+            trajectories = list(
+                tqdm(pool.imap(read_trajectory, fpaths), total=len(fpaths))
+            )
 
     return np.array(trajectories, dtype=np.float32)
 
@@ -62,10 +66,11 @@ def load_trajectories(dataset, parallel=True):
 class TrajectoryDataset(torch.utils.data.Dataset):
     normed_trajectories = None
 
-    def __init__(self,
+    def __init__(
+        self,
         dataset=None,
         horizon=64,
-        normalizer='LimitsNormalizer',
+        normalizer="LimitsNormalizer",
         preprocess_fns=(),
         max_path_length=1000,
         use_padding=True,
@@ -75,7 +80,7 @@ class TrajectoryDataset(torch.utils.data.Dataset):
         self.use_padding = use_padding
 
         if dataset is None:
-            raise ValueError('dataset not specified')
+            raise ValueError("dataset not specified")
 
         trajectories = load_trajectories(dataset)
 
@@ -99,16 +104,18 @@ class TrajectoryDataset(torch.utils.data.Dataset):
 
     def normalize(self):
         """
-            normalize fields that will be predicted by the diffusion model
+        normalize fields that will be predicted by the diffusion model
         """
-        array = self.trajectories.reshape(self.n_episodes*self.max_path_length, -1)
+        array = self.trajectories.reshape(self.n_episodes * self.max_path_length, -1)
         normed = self.normalizer(array)
-        self.normed_trajectories = normed.reshape(self.n_episodes, self.max_path_length, -1)
+        self.normed_trajectories = normed.reshape(
+            self.n_episodes, self.max_path_length, -1
+        )
 
     def make_indices(self, path_lengths, horizon):
         """
-            makes indices for sampling from dataset;
-            each index maps to a datapoint
+        makes indices for sampling from dataset;
+        each index maps to a datapoint
         """
         indices = []
         for i, path_length in enumerate(path_lengths):
@@ -122,9 +129,9 @@ class TrajectoryDataset(torch.utils.data.Dataset):
         return indices
 
     def get_conditions(self, observations):
-        '''
-            condition on current observation for planning
-        '''
+        """
+        condition on current observation for planning
+        """
         return {0: observations[0]}
 
     def __len__(self):

mg_diffuse/utils/data_preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 
+
 def handle_trajectory_angle_wraparound(original_trajectory):
     correction = 0  # the value of correction is either 2pi or -2pi
 
@@ -8,21 +9,28 @@ def handle_trajectory_angle_wraparound(original_trajectory):
 
     for state_idx in range(1, trajectory.shape[0]):
 
-        if original_trajectory[state_idx, 0] - original_trajectory[state_idx - 1, 0] >= np.pi:
-            correction += -2 * np.pi
+        if (
+            original_trajectory[state_idx, 0] - original_trajectory[state_idx - 1, 0]
+            >= np.pi
+        ):
+            correction -= 2 * np.pi
 
-        elif original_trajectory[state_idx, 0] - original_trajectory[state_idx - 1, 0] <= -np.pi:
+        elif (
+            original_trajectory[state_idx, 0] - original_trajectory[state_idx - 1, 0]
+            <= -np.pi
+        ):
             correction += 2 * np.pi
 
         trajectory[state_idx, 0] += correction
 
     return trajectory
 
+
 def handle_angle_wraparound(trajectories, parallel=True):
     """
-        handle angle wraparound
+    handle angle wraparound
     """
-    print(f'[ utils/data_preprocessing ] Handling angle wraparound')
+    print(f"[ utils/data_preprocessing ] Handling angle wraparound")
     updated_trajectories = []
     if not parallel:
         for trajectory in tqdm(trajectories):
@@ -32,13 +40,17 @@ def handle_angle_wraparound(trajectories, parallel=True):
 
         # read trajectories in parallel with tqdm progress bar
         with mp.Pool(mp.cpu_count()) as pool:
-            updated_trajectories = list(tqdm(pool.imap(handle_trajectory_angle_wraparound, trajectories), total=len(trajectories)))
+            updated_trajectories = list(
+                tqdm(
+                    pool.imap(handle_trajectory_angle_wraparound, trajectories),
+                    total=len(trajectories),
+                )
+            )
 
     # for trajectory in updated_trajectories:
     #     for state_idx in range(1, trajectory.shape[0]):
     #         if abs(trajectory[state_idx, 0] - trajectory[state_idx - 1, 0]) >= np.pi:
     #             print('Error: angle wraparound not handled correctly')
     #             breakpoint()
-
 
     return np.array(updated_trajectories)