e3034ff6ca6f0d69b05b6a80ea49090d3411b71c
sumanth-tangirala <tangiralasumanth@gmail.com>
2025-06-05 00:35:38 -0400

    Moved RoA estimator to the evaluation directory

--- genMoPlan/utils/__init__.py
+++ genMoPlan/utils/__init__.py
@@ -1,3 +1,7 @@
+import os
+import glob
+
+
 from .arrays import *
 from .cloud import *
 from .config import *
@@ -8,7 +12,6 @@ from .manifold import *
 from .model import *
 from .params import *
 from .progress import *
-from .roa import *
 from .serialization import *
 from .setup import *
 from .timer import *
@@ -18,3 +21,38 @@ from .trajectory import *
 
 def generate_timestamp():
     return time.strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def has_best_pt(directory):
+    return os.path.exists(os.path.join(directory, "best.pt"))
+
+
+def expand_model_paths(model_paths):
+    expanded_paths = []
+    
+    for path in model_paths:
+        if "*" in path:
+            # Handle wildcard paths
+            # Use glob.glob directly with the path containing the wildcard
+            all_dirs = glob.glob(path)
+            
+            # Filter directories that contain best.pt
+            valid_dirs = [d for d in all_dirs if os.path.isdir(d) and has_best_pt(d)]
+            
+            # Add valid directories to expanded paths
+            expanded_paths.extend(valid_dirs)
+        else:
+            # Direct path without wildcard
+            if os.path.exists(path) and has_best_pt(path):
+                expanded_paths.append(path)
+            else:
+                print(f"Warning: Path {path} does not exist or does not contain best.pt")
+
+    print(f"Found {len(expanded_paths)} valid model paths:")
+    for model_path in expanded_paths:
+        print(f"  - {model_path}")
+
+    input("Press Enter to continue...")
+    
+    return expanded_paths
+
