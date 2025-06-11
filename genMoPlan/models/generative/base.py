eb9a7271225ab50f6edc6009c10694898afad949
sumanth-tangirala <tangiralasumanth@gmail.com>
2025-06-11 11:27:19 -0400

    Added support for local queries and also introduced a new transformer architecture

--- genMoPlan/models/generative/base.py
+++ genMoPlan/models/generative/base.py
@@ -4,7 +4,7 @@ from typing import Union
 from torch import nn
 import torch
 
-from genMoPlan.utils import ManifoldWrapper
+from genMoPlan.models.temporal.base import TemporalModel
 
 from ..helpers import (
     get_loss_weights,
@@ -26,20 +26,22 @@ class GenerativeModel(nn.Module, ABC):
         loss_weights=None,
         loss_discount=1.0,
         action_indices=None,
-        has_query=False,
+        has_global_query=False,
+        has_local_query=False,
         manifold=None,
         **kwargs,
     ):
         super().__init__()
 
-        self.model = model
+        self.model: TemporalModel = model
 
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.clip_denoised = clip_denoised
         self.prediction_length = prediction_length
         self.action_indices = action_indices
-        self.has_query = has_query
+        self.has_global_query = has_global_query
+        self.has_local_query = has_local_query
         self.register_buffer("loss_weights", get_loss_weights(output_dim, prediction_length, loss_discount, loss_weights))
         self.manifold = manifold
         self.loss_fn = Losses[loss_type](history_length, action_indices, manifold=manifold)
@@ -61,25 +63,31 @@ class GenerativeModel(nn.Module, ABC):
         """
         raise NotImplementedError("Subclasses must implement this method")
     
-    def loss(self, x, cond, query):
-        if not self.has_query:
-            query = None
+    def loss(self, x, cond, global_query=None, local_query=None):
+        if not self.has_local_query:
+            local_query = None
 
-        return self.compute_loss(x, cond, query)
+        if not self.has_global_query:
+            global_query = None
+
+        return self.compute_loss(x, cond, global_query, local_query)
 
     @torch.no_grad()
-    def forward(self, cond, query=None, verbose=True, return_chain=False, **kwargs):
+    def forward(self, cond, global_query=None, local_query=None, verbose=True, return_chain=False, **kwargs):
         batch_size = len(cond[0])
         shape = (batch_size, self.prediction_length, self.output_dim)
-        return self.conditional_sample(cond, shape, query=query, verbose=verbose, return_chain=return_chain, **kwargs) 
+        return self.conditional_sample(cond, shape, global_query=global_query, local_query=local_query, verbose=verbose, return_chain=return_chain, **kwargs) 
     
 
     @torch.no_grad()
-    def validation_loss(self, x, cond, query, **kwargs):
+    def validation_loss(self, x, cond, global_query=None, local_query=None, **kwargs):
         """
         Compute the validation loss for the model.
         """
-        if not self.has_query:
-            query = None
+        if not self.has_local_query:
+            local_query = None
 
-        return self.compute_loss(x, cond, query)
+        if not self.has_global_query:
+            global_query = None
+
+        return self.compute_loss(x, cond, global_query, local_query)
